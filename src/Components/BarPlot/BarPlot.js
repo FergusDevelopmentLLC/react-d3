a167399a3f1f121fe74d901c40029815f62223b5
Will Carter <will.carter@fergusllc.com>
2019-10-10 14:17:35 -0600

    refactor

--- src/Components/BarPlot/BarPlot.js
+++ src/Components/BarPlot/BarPlot.js
@@ -26,36 +26,19 @@ export const BarPlot = ({
   onSelectItem
 }) => {
 
-  if (!data) {
-    return <pre>Loading...</pre>;
-  }
+  if (!data) { return <pre>Loading...</pre> }
 
-  const margin = { 
-    left: `${svgWidth * 0.12}`,
-    right: `${svgWidth * 0.06}`, 
-    top: `${svgHeight * 0.12}`, 
-    bottom: `${tiltXLabels ? svgHeight * 0.25 : svgHeight * 0.15}`
-  }
-
+  const margin = { left: `${svgWidth * 0.12}`, right: `${svgWidth * 0.06}`, top: `${svgHeight * 0.12}`, bottom: `${tiltXLabels ? svgHeight * 0.25 : svgHeight * 0.15}` }
   const chartWidth = svgWidth - margin.right - margin.left;
   const chartHeight = svgHeight - margin.top - margin.bottom;
 
-  //reset the opacity of the breaks to 100% for the vis
-  for (let color of colorBreaks) {
-    color['rgba'] = `rgba(${color.rgba[0]},${color.rgba[1]},${color.rgba[2]},1)`
-  }
-
   //domain array length doesn't include 0 index 1 ... n, range array includes all elements 0 ... n
-  const colorScale = d3.scaleThreshold()
-    .domain(colorBreaks.map(b => b.break).slice(1, colorBreaks.length))
-    .range(colorBreaks.map(c => c.rgba))
-
+  const colorScale = d3.scaleThreshold().domain(colorBreaks.map(b => b.break).slice(1, colorBreaks.length)).range(colorBreaks.map(c => `rgba(${c.rgba[0]},${c.rgba[1]},${c.rgba[2]},1)`))
+  
   const xScale = d3.scaleBand().range([0, chartWidth]).padding(0.1).domain(data.map(d => d.type))
+  const yScale = d3.scaleLinear().range([chartHeight, 0]).domain([0, d3.max(data.map(d => d.count))]).nice()
   
-  const countMax = d3.max(data.map(d => d.count))
-  const yScale = d3.scaleLinear().range([chartHeight, 0]).domain([0, countMax]).nice()
-  
-  const minHeight = 2.5//set a minHeight for a bar so that it is visible even if very small. Bad?
+  const minHeight = 2.5//set a minHeight for a bar so that it is selectable
 
   return (
     <svg width={svgWidth} height={svgHeight} style={styles}>
